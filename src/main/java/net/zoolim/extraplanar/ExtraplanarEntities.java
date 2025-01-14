25276656651f02d095fab4161a68e14fee818668
supersonic1022 <82128093+supersonic1022@users.noreply.github.com>
2025-01-14 02:12:30 -0500

    Initial commit

---
+++ src/main/java/net/zoolim/extraplanar/ExtraplanarEntities.java
@@ -0,0 +1,16 @@
+package net.zoolim.extraplanar;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExtraplanarEntities implements ModInitializer {
+	public static final String MOD_ID = "extraplanarentities";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/zoolim/extraplanar/ExtraplanarEntitiesClient.java
@@ -0,0 +1,10 @@
+package net.zoolim.extraplanar;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExtraplanarEntitiesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
